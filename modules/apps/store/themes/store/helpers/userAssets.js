db31b57c2570cb4fadf4ce13323cce3ec0386ab8
madhuka <madhukaudantha@gmail.com>
2013-07-05 19:41:50 +0530

    adding pagination js

--- modules/apps/store/themes/store/helpers/userAssets.js
+++ modules/apps/store/themes/store/helpers/userAssets.js
@@ -1,6 +1,6 @@
 var resources = function (page, meta) {
     return {
         css: ['userAssets.css'],
-       	js: ['userAssets.js']
+       	js: ['userAssets.js', 'jquery.event.mousestop.js','jquery.carouFredSel-6.2.1-packed.js']
     };
 };
