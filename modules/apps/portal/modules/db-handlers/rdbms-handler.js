b7229a69f5542da00dc2af8ae1ac40d235e9f39f
TanyaM <tanyamadurapperuma@gmail.com>
2014-04-04 10:24:00 +0530

    Fixing UES-518

--- modules/apps/portal/modules/db-handlers/rdbms-handler.js
+++ modules/apps/portal/modules/db-handlers/rdbms-handler.js
@@ -1,7 +1,32 @@
+function getDataSources() {
+    var isConSuccess = true;
+
+    var dataSources = [];
+
+    var log = new Log();
+    var DataSourceManager = Packages.org.wso2.carbon.ndatasource.core.DataSourceManager;
+    var mydatasourceManager = new DataSourceManager();
+
+    var coll = mydatasourceManager.getInstance().getDataSourceRepository().getAllDataSources();
+    var iterator = coll.iterator();
+
+    while (iterator.hasNext()) {
+        var dataSrc = iterator.next().getDSMInfo().getName();
+        dataSources.push(dataSrc);
+    }
+
+
+    return dataSources;
+}
+
 function validateConnection(connectionSettings) {
     var isConSuccess = true;
     try {
-        var db = new Database(connectionSettings['Connection URL'], connectionSettings['Username'], connectionSettings['Password']);
+        if(connectionSettings['Connection']){
+            var db = new Database(connectionSettings['Connection']);
+        }else{
+            var db = new Database(connectionSettings['Connection URL'], connectionSettings['Username'], connectionSettings['Password']);
+        }
     } catch (e) {
         isConSuccess = false;
     }
@@ -10,8 +35,13 @@ function validateConnection(connectionSettings) {
 
 function getData(connectionSettings, sql_statement) {
     var dbResult;
+    var db;
     try {
-        var db = new Database(connectionSettings['Connection URL'], connectionSettings['Username'], connectionSettings['Password']);
+        if(connectionSettings['Connection']){
+            db = new Database(connectionSettings['Connection']);
+        }else{
+            db = new Database(connectionSettings['Connection URL'], connectionSettings['Username'], connectionSettings['Password']);
+        }
         dbResult = db.query(sql_statement['SQL Statement']);
         return dbResult;
         //if data is not in the tabular format it needs to be formatted here in other handlers
