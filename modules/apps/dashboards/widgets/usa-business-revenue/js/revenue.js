a14a9f3a6c2305e28042ef99485f2ad934e5688d
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-12 20:21:58 +0530

    linking sample gadgets

--- modules/apps/dashboards/widgets/usa-business-revenue/js/revenue.js
+++ modules/apps/dashboards/widgets/usa-business-revenue/js/revenue.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 500 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = 400 - margin.left - margin.right,
+    height = 280 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
     .rangeRoundBands([0, width], .1);
