f76de0bd69023ccadd9a1be2d0dcf3a85c4a650f
ruchiraw <ruchira.wageesha@gmail.com>
2015-02-19 10:06:56 +0530

    updating styles

--- modules/apps/dashboards/js/shind.js
+++ modules/apps/dashboards/js/shind.js
@@ -1,15 +1,5 @@
 (function () {
-    var container = $('.gadget');
-    var url = 'http://localhost:9763/dashboards/widgets/usa-map/usa.xml';
-    var site = CommonContainer.newGadgetSite(container[0]);
-    var params = {};
-    params[osapi.container.RenderParam.WIDTH] = '100%';
-    params[osapi.container.RenderParam.VIEW] = 'home';
-    params[osapi.container.RenderParam.HEIGHT] = container.height();
-    CommonContainer.navigateGadget(site, url, {}, params, function (metadata) {
-        if (metadata.error) {
-            gadgets.error('There was an error rendering ' + url);
-            return;
-        }
-    });
+    var container = $('.ues-widget-box').first();
+    var url = 'http://localhost:9763/dashboards/widgets/usa-map/index.xml';
+    CommonContainer.renderGadget(container, url);
 }());
