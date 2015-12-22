45b8ace2fd42fa92c088371524047be1be8a7bae
nipunamarcus <nipunamarcus2@gmail.com>
2015-12-22 17:58:43 +0530

    Fixed test failures

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/DashboardAnonViewTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/DashboardAnonViewTest.java
@@ -27,6 +27,8 @@ import org.testng.annotations.*;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.ds.ui.integration.util.DSUIIntegrationTest;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
@@ -36,7 +38,16 @@ import static org.testng.Assert.assertTrue;
 public class DashboardAnonViewTest extends DSUIIntegrationTest {
     private static final Log LOG = LogFactory.getLog(DashboardAnonViewTest.class);
     private static final String DASHBOARD_TITLE = "anondashboard";
-    private static final String DASHBOARD_DESCRIPTION = "This is sample descrition for dashboard";
+    private static final String DASHBOARD_DESCRIPTION = "This is sample description for dashboard";
+    private static final String GADGET_1 = "USA Map";
+    private static final String GADGET_1_ID = "usa-map";
+    private static final String GADGET_2 = "Publisher";
+    private static final String GADGET_2_ID = "publisher";
+    private static final String GADGET_3 = "USA Social";
+    private static final String GADGET_3_ID = "usa-social";
+    private static final String GADGET_4 = "Subscriber";
+    private static final String GADGET_4_ID = "subscriber";
+    private static final String CONTAINER_A = "a";
     private String dashboardTitle;
 
     @Factory(dataProvider = "userMode")
@@ -61,95 +72,42 @@ public class DashboardAnonViewTest extends DSUIIntegrationTest {
      */
     @Test(groups = "wso2.ds.dashboard", description = "Adding anonymous dashboard for dashboard")
     public void testAnonDashboard() throws Exception {
-        String defaultViewGadgetExpected = "USA Map";
-        String anonViewGadgetExpected = "G1";
+        String[][] anonViewGadgetMappings = {{GADGET_2_ID, CONTAINER_A}};
+        String[][] defaultViewGadgetMappings = {{GADGET_1_ID, CONTAINER_A}};
+        String anonViewGadgetAddScript = generateAddGadgetScript(anonViewGadgetMappings);
+        String defaultViewGadgetAddScript = generateAddGadgetScript(defaultViewGadgetMappings);
 
-        getDriver().get(getBaseUrl() + "/portal/dashboards");
-        getDriver().findElement(By.cssSelector("[href='create-dashboard']")).click();
-
-        getDriver().findElement(By.id("ues-dashboard-title")).clear();
-        getDriver().findElement(By.id("ues-dashboard-title")).sendKeys(dashboardTitle);
-        getDriver().findElement(By.id("ues-dashboard-create")).click();
-        getDriver().findElement(By.cssSelector("#default-grid")).click();
-        getDriver().findElement(By.cssSelector(".ues-page-properties-toggle")).click();
+        addDashBoard(dashboardTitle, DASHBOARD_DESCRIPTION);
+        getDriver().findElement(By.cssSelector("#" + dashboardTitle.toLowerCase() + " .ues-edit")).click();
+        getDriver().findElement(By.cssSelector("a.ues-pages-toggle")).click();
+        getDriver().findElement(By.cssSelector("a[data-id='landing']")).click();
         getDriver().findElement(By.cssSelector("input[name='landing']")).click();
         getDriver().findElement(By.cssSelector("input[name='anon']")).click();
         getDriver().findElement(By.className("toggle-group")).click();
         getDriver().findElement(By.cssSelector("a[data-type='gadget']")).click();
+        getDriver().executeScript(anonViewGadgetAddScript);
 
-        ((JavascriptExecutor) getDriver()).executeScript(" var draggableElement = $(\"div[data-id='usa-map']\")" +
-                ".draggable({cancel: false," +
-                "appendTo: 'body'," +
-                "helper: 'clone'," +
-                "start: function (event, ui) {" +
-                "ui.helper.addClass('ues-store-thumbnail');}," +
-                "stop: function (event, ui) {" +
-                "ui.helper.removeClass('ues-store-thumbnail');" +
-                "}})," +
-                "dropableElement=$('#a');" +
-                "var droppableOffset = dropableElement.offset()," +
-                "draggableOffset = draggableElement.offset()," +
-                "dx = droppableOffset.left - draggableOffset.left," +
-                "dy = droppableOffset.top - draggableOffset.top;" +
-                "draggableElement.simulate('drag', {" +
-                "dx: dx," +
-                "dy: dy" +
-                "});");
-
-        String defaultDesignerModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        assertEquals(GADGET_2, getAttributeValue("iframe", "title"));
 
         getDriver().findElement(By.className("toggle-group")).click();
-        ((JavascriptExecutor) getDriver()).executeScript(" var draggableElement = $(\"div[data-id='g1']\")" +
-                ".draggable({cancel: false," +
-                "appendTo: 'body'," +
-                "helper: 'clone'," +
-                "start: function (event, ui) {" +
-                "ui.helper.addClass('ues-store-thumbnail');}," +
-                "stop: function (event, ui) {" +
-                "ui.helper.removeClass('ues-store-thumbnail');" +
-                "}})," +
-                "dropableElement=$('#a');" +
-                "var droppableOffset = dropableElement.offset()," +
-                "draggableOffset = draggableElement.offset()," +
-                "dx = droppableOffset.left - draggableOffset.left," +
-                "dy = droppableOffset.top - draggableOffset.top;" +
-                "draggableElement.simulate('drag', {" +
-                "dx: dx," +
-                "dy: dy" +
-                "});");
+        getDriver().executeScript(defaultViewGadgetAddScript);
 
-        String anonDesignerModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
-        String previousWinHandler = getDriver().getWindowHandle();
+        assertEquals(GADGET_1, getAttributeValue("iframe", "title"));
+
         getDriver().findElement(By.className("ues-dashboard-preview")).click();
 
-        for (String windowHandl : getDriver().getWindowHandles()) {
-            if (!windowHandl.contains(previousWinHandler)) {
-                getDriver().switchTo().window(windowHandl);
-                break;
-            }
-        }
-
-        String anonViewModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        pushWindow();
+        assertEquals(GADGET_1, getAttributeValue("iframe", "title"));
         getDriver().close();
-        getDriver().switchTo().window(previousWinHandler);
+        popWindow();
+
         getDriver().findElement(By.className("toggle-group")).click();
         getDriver().findElement(By.className("ues-dashboard-preview")).click();
 
-        for (String windowHandl : getDriver().getWindowHandles()) {
-            if (!windowHandl.contains(previousWinHandler)) {
-                getDriver().switchTo().window(windowHandl);
-                break;
-            }
-        }
-
-        String defaultViewModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        pushWindow();
+        assertEquals(GADGET_2, getAttributeValue("iframe", "title"));
         getDriver().close();
-        getDriver().switchTo().window(previousWinHandler);
-
-        assertEquals(anonViewGadgetExpected, anonDesignerModeGadgetActual);
-        assertEquals(defaultViewGadgetExpected, defaultDesignerModeGadgetActual);
-        assertEquals(anonViewGadgetExpected, anonViewModeGadgetActual);
-        assertEquals(defaultViewGadgetExpected, defaultViewModeGadgetActual);
+        popWindow();
     }
 
     /**
@@ -158,108 +116,47 @@ public class DashboardAnonViewTest extends DSUIIntegrationTest {
     @Test(groups = "wso2.ds.dashboard", description = "Adding anonymous dashboard page for dashboard",
             dependsOnMethods = "testAnonDashboard")
     public void testAnonDashboardPages() throws Exception {
-        String anonGadgetExpected = "USA Social";
-        String defaultGadgetExpected = "G2";
+        String[][] anonViewGadgetMappings = {{GADGET_4_ID, CONTAINER_A}};
+        String[][] defaultViewGadgetMappings = {{GADGET_3_ID, CONTAINER_A}};
+        String anonViewGadgetAddScript = generateAddGadgetScript(anonViewGadgetMappings);
+        String defaultViewGadgetAddScript = generateAddGadgetScript(defaultViewGadgetMappings);
 
-        getDriver().findElement(By.className("ues-page-add")).click();
-        getDriver().findElement(By.cssSelector("#default-grid")).click();
-        getDriver().findElement(By.cssSelector(".ues-page-properties-toggle")).click();
+        addPageToDashboard();
         getDriver().findElement(By.cssSelector("input[name='anon']")).click();
         getDriver().findElement(By.className("toggle-group")).click();
+        getDriver().executeScript(anonViewGadgetAddScript);
 
-        ((JavascriptExecutor) getDriver()).executeScript(" var draggableElement = $(\"div[data-id='g2']\")" +
-                ".draggable({cancel: false," +
-                "appendTo: 'body'," +
-                "helper: 'clone'," +
-                "start: function (event, ui) {" +
-                "ui.helper.addClass('ues-store-thumbnail');}," +
-                "stop: function (event, ui) {" +
-                "ui.helper.removeClass('ues-store-thumbnail');" +
-                "}})," +
-                "dropableElement=$('#a');" +
-                "var droppableOffset = dropableElement.offset()," +
-                "draggableOffset = draggableElement.offset()," +
-                "dx = droppableOffset.left - draggableOffset.left," +
-                "dy = droppableOffset.top - draggableOffset.top;" +
-                "draggableElement.simulate('drag', {" +
-                "dx: dx," +
-                "dy: dy" +
-                "});");
-
-        String defaultPageGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        assertEquals(GADGET_4, getAttributeValue("iframe", "title"));
 
         getDriver().findElement(By.className("toggle-group")).click();
-        ((JavascriptExecutor) getDriver()).executeScript(" var draggableElement = $(\"div[data-id='usa-social']\")" +
-                ".draggable({cancel: false," +
-                "appendTo: 'body'," +
-                "helper: 'clone'," +
-                "start: function (event, ui) {" +
-                "ui.helper.addClass('ues-store-thumbnail');}," +
-                "stop: function (event, ui) {" +
-                "ui.helper.removeClass('ues-store-thumbnail');" +
-                "}})," +
-                "dropableElement=$('#a');" +
-                "var droppableOffset = dropableElement.offset()," +
-                "draggableOffset = draggableElement.offset()," +
-                "dx = droppableOffset.left - draggableOffset.left," +
-                "dy = droppableOffset.top - draggableOffset.top;" +
-                "draggableElement.simulate('drag', {" +
-                "dx: dx," +
-                "dy: dy" +
-                "});");
+        getDriver().executeScript(defaultViewGadgetAddScript);
 
-        String anonPageGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        assertEquals(GADGET_3, getAttributeValue("iframe", "title"));
 
-        getDriver().findElement(By.className("ues-page-switcher")).click();
+        switchPage("landing");
+        switchPage("page0");
 
-        getDriver().findElement(By.cssSelector("a[data-id='landing']")).click();
-
-        getDriver().findElement(By.className("ues-page-switcher")).click();
-
-        getDriver().findElement(By.cssSelector("a[data-id='page0']")).click();
-
-        String switchedPagedDefaultGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        assertEquals(GADGET_3, getAttributeValue("iframe", "title"));
 
         getDriver().findElement(By.className("toggle-group")).click();
 
-        String switchedPageAnonGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
-
-        getDriver().findElement(By.className("toggle-group")).click();
-
-        String previousWinHandler = getDriver().getWindowHandle();
-        getDriver().findElement(By.className("ues-dashboard-preview")).click();
-
-        for (String windowHandl : getDriver().getWindowHandles()) {
-            if (!windowHandl.contains(previousWinHandler)) {
-                getDriver().switchTo().window(windowHandl);
-                break;
-            }
-        }
-
-        String defaultViewModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
-        getDriver().close();
-        getDriver().switchTo().window(previousWinHandler);
+        assertEquals(GADGET_4, getAttributeValue("iframe", "title"));
 
         getDriver().findElement(By.className("toggle-group")).click();
         getDriver().findElement(By.className("ues-dashboard-preview")).click();
 
-        for (String windowHandl : getDriver().getWindowHandles()) {
-            if (!windowHandl.contains(previousWinHandler)) {
-                getDriver().switchTo().window(windowHandl);
-                break;
-            }
-        }
-
-        String anonViewModeGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
+        pushWindow();
+        assertEquals(GADGET_3, getAttributeValue("iframe", "title"));
         getDriver().close();
-        getDriver().switchTo().window(previousWinHandler);
+        popWindow();
 
-        assertEquals(anonGadgetExpected, anonPageGadgetActual);
-        assertEquals(defaultGadgetExpected, defaultPageGadgetActual);
-        assertEquals(anonGadgetExpected, switchedPageAnonGadgetActual);
-        assertEquals(defaultGadgetExpected, switchedPagedDefaultGadgetActual);
-        assertEquals(anonGadgetExpected, anonViewModeGadgetActual);
-        assertEquals(defaultGadgetExpected, defaultViewModeGadgetActual);
+        getDriver().findElement(By.className("toggle-group")).click();
+        getDriver().findElement(By.className("ues-dashboard-preview")).click();
+
+        pushWindow();
+        assertEquals(GADGET_4, getAttributeValue("iframe", "title"));
+        getDriver().close();
+        popWindow();
     }
 
     /**
@@ -268,17 +165,18 @@ public class DashboardAnonViewTest extends DSUIIntegrationTest {
     @Test(groups = "wso2.ds.dashboard", description = "Remove anonymous view mode from added dashboard page in dashboard",
             dependsOnMethods = "testAnonDashboardPages")
     public void testAnonDashboardPageRemove() throws Exception {
-        String defaultGadgetExpected = "G2";
-
+        switchPage("page0");
         getDriver().findElement(By.cssSelector("input[name='anon']")).click();
         boolean isToggleButtonHidden = getDriver().findElements(By.cssSelector(".toggle-design-view.hide")).size() > 0;
-        String defaultGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
-        getDriver().get(getBaseUrl() + "/portal/dashboards/" + dashboardTitle + "/landing?isAnonView=true");
+
+        assertTrue(isToggleButtonHidden, "Anonymous toggle button is not hidden");
+        assertEquals(GADGET_3, getAttributeValue("iframe", "title"));
+
+        redirectToLocation("portal", "dashboards/" + dashboardTitle + "/landing?isAnonView=true");
+        modifyTimeOut(2);
         boolean isLinkForNonAnonPageNotExist = getDriver().findElements(By.cssSelector("a[href='" + getBaseUrl() +
                 "/portal/dashboards/" + dashboardTitle + "/page0?isAnonView=true']")).size() <= 0;
-
-        assertEquals(defaultGadgetExpected, defaultGadgetActual);
-        assertTrue(isToggleButtonHidden, "Anonymous toggle button is not hidden");
+        resetTimeOut();
         assertTrue(isLinkForNonAnonPageNotExist, "Link for the non anon page is still available");
     }
 
@@ -288,22 +186,31 @@ public class DashboardAnonViewTest extends DSUIIntegrationTest {
     @Test(groups = "wso2.ds.dashboard", description = "Remove anonymous view mode form dashboard",
             dependsOnMethods = "testAnonDashboardPageRemove")
     public void testRemoveAnonModeFromDashboard() throws Exception {
-        String defaultViewGadgetExpected = "USA Map";
-
-        getDriver().get(getBaseUrl() + "/portal/dashboards/" + dashboardTitle + "?editor=true");
-        getDriver().findElement(By.cssSelector(".ues-page-properties-toggle")).click();
+        redirectToLocation("portal", "dashboards/" + dashboardTitle + "?editor=true");
+        getDriver().findElement(By.cssSelector("a.ues-pages-toggle")).click();
+        switchPage("landing");
         getDriver().findElement(By.cssSelector("input[name='anon']")).click();
-
         boolean isToggleButtonHidden = getDriver().findElements(By.cssSelector(".toggle-design-view.hide")).size() > 0;
-        String defaultGadgetActual = getDriver().findElement(By.cssSelector("iframe")).getAttribute("title");
-        getDriver().get(getBaseUrl() + "/portal/dashboards/" + dashboardTitle + "/landing?isAnonView=true");
-        boolean noIFramesAvailable = getDriver().findElements(By.cssSelector("iframe")).size() <= 0;
 
-        getDriver().close();
-
-        assertEquals(defaultViewGadgetExpected, defaultGadgetActual);
         assertTrue(isToggleButtonHidden, "Anonymous toggle button is not hidden");
+        assertEquals(GADGET_1, getAttributeValue("iframe", "title"));
+
+        redirectToLocation("portal", "dashboards/" + dashboardTitle + "/landing?isAnonView=true");
+        modifyTimeOut(2);
+        boolean noIFramesAvailable = getDriver().findElements(By.cssSelector("iframe")).size() <= 0;
+        resetTimeOut();
         assertTrue(noIFramesAvailable, "There are gadgets available for anonymous view when no anonymous view available");
+    }
+
+    /**
+     * Get the value of given attribute of given element
+     *
+     * @param element       name of the element
+     * @param attributeName name of the attribute
+     * @return {String}
+     */
+    public String getAttributeValue(String element, String attributeName) throws Exception {
+        return getDriver().findElement(By.cssSelector(element)).getAttribute(attributeName);
     }
 
     @AfterClass(alwaysRun = true)
