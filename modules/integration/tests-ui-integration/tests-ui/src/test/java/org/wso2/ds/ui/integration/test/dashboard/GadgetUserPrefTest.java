174c30ed1373c39459c41c2f51fb4ea9717717ef
nisalaniroshana <nisalaniroshana@gmail.com>
2016-04-25 17:39:47 +0530

    Refactored the code

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/GadgetUserPrefTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/GadgetUserPrefTest.java
@@ -223,17 +223,14 @@ public class GadgetUserPrefTest extends DSUIIntegrationTest {
         driver.findElement(By.id("ues-share-view")).clear();
         driver.findElement(By.id("ues-share-view")).sendKeys("viewer_userPr");
         driver.findElement(By.id("ues-share-view")).sendKeys(Keys.TAB);
-
         driver.findElement(By.id("ues-share-edit")).clear();
         driver.findElement(By.id("ues-share-edit")).sendKeys("editor_userPr");
         driver.findElement(By.id("ues-share-edit")).sendKeys(Keys.TAB);
-
         // Remove other permissions
         driver.findElement(By.cssSelector(".ues-shared-view " +
                 ".ues-shared-role[data-role=\"Internal/everyone\"] span.remove-button")).click();
         driver.findElement(By.cssSelector(".ues-shared-edit " +
                 ".ues-shared-role[data-role=\"Internal/everyone\"] span.remove-button")).click();
-
         driver.findElement(By.id("ues-dashboard-saveBtn")).click();
         redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
 
