9de47818e9520ca1e7b354406c13b0926fa42a94
prabushi <prabushisamarakoon@gmail.com>
2016-05-26 11:01:59 +0530

    added test for applying custom theme

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/AddCustomThemeToDashboardTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/AddCustomThemeToDashboardTest.java
@@ -1,10 +1,11 @@
 package org.wso2.ds.ui.integration.test.dashboard;
 
 import org.openqa.selenium.By;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Factory;
-import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
 import org.wso2.ds.ui.integration.util.DSUIIntegrationTest;
@@ -13,12 +14,17 @@ import javax.xml.xpath.XPathExpressionException;
 import java.io.File;
 import java.net.MalformedURLException;
 
+import static org.testng.Assert.assertEquals;
+
 /**
  * Created by prabushi on 5/25/16.
  */
 public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
+    private static final Log log = LogFactory.getLog(AddCustomThemeToDashboardTest.class);
     private static final String USER_NAME = "admin";
     private static final String PASSWORD = "admin";
+    private static final String dashboardTitle = "sample-dashboard";
+    private static final String description = "This is a sample dashboard";
     private static final String CUSTOM_THEME_NAME = "custom-theme-sample";
     //get the path to custom theme carbon archive file
     String systemResourceLocation = FrameworkPathUtil.getSystemResourceLocation();
@@ -31,9 +37,17 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
             File.separator + "store" + File.separator + "carbon.super" + File.separator + "fs" +
             File.separator + "theme" + File.separator + CUSTOM_THEME_NAME;
 
-    @Factory(dataProvider = "userMode")
-    public AddCustomThemeToDashboardTest(TestUserMode userMode) {
+    @Factory(dataProvider = "userMode") public AddCustomThemeToDashboardTest(TestUserMode userMode) {
         super(userMode);
+    }
+
+    /**
+     * Provides user modes.
+     *
+     * @return user modes
+     */
+    @DataProvider(name = "userMode") public static Object[][] userModeProvider() {
+        return new Object[][] { { TestUserMode.SUPER_TENANT_ADMIN } };
     }
 
     /**
@@ -58,140 +72,55 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
         logoutFromAdminConsole();
     }
 
-    @Test(groups = "wso2.ds.dashboard", description = "Adding a custom theme when creating a dashboard")
-    public void addCustomThemeToDashboard() throws XPathExpressionException, MalformedURLException {
+    @Test(groups = "wso2.ds.dashboard", description = "Adding a custom theme when creating a dashboard") public void addCustomThemeToDashboard()
+            throws XPathExpressionException, MalformedURLException, InterruptedException {
         //log in to portal
         login(getCurrentUsername(), getCurrentPassword());
 
+        //create dashboard
+        redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
+        getDriver().findElement(By.cssSelector("[href='create-dashboard']")).click();
+        getDriver().findElement(By.id("ues-dashboard-title")).clear();
+        getDriver().findElement(By.id("ues-dashboard-title")).sendKeys(dashboardTitle);
+        getDriver().findElement(By.id("ues-dashboard-description")).clear();
+        getDriver().findElement(By.id("ues-dashboard-description")).sendKeys(description);
+        new Select(getDriver().findElement(By.id("ues-theme-list"))).selectByVisibleText("custom-theme-sample");
+        getDriver().findElement(By.id("ues-dashboard-create")).click();
+        selectLayout("default-grid");
+
+        //add a page to the dashboard
+        redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
+        WebElement webElement = getDriver().findElement(By.id(dashboardTitle.toLowerCase()));
+        webElement.findElement(By.cssSelector(".ues-edit")).click();
+        addPageToDashboard();
+
+        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + dashboardTitle + "/landing");
+        String x = getDriver().findElement(By.cssSelector(".product-name")).getText();
+        log.info("$$$$" + x);
+        assertEquals(getDriver().findElement(By.cssSelector(".product-name")).getText(),
+                "This is a custom theme title");
+
+        redirectToLocation(DS_HOME_CONTEXT, "dashboard-settings/" + dashboardTitle);
+        //edirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
+        //bElement = getDriver().findElement(By.id(dashboardTitle.toLowerCase()));
+        //getDriver().findElement(By.id("dashboard-settings")).click();
+        //getDriver().findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        //change custom theme to default theme through dashboard settings
+        //getDriver().findElement(By.linkText("Default Theme")).click();
+        //getDriver().findElement(By.id("ues-dashboard-theme")).sendKeys("Default Theme");
+        getDriver().findElement(By.xpath("(//button[@type='button'])[2]")).click();
+        getDriver().findElement(By.linkText("Default Theme")).click();
+        getDriver().findElement(By.id("ues-dashboard-saveBtn")).click();
+        Thread.sleep(10000);
+        //redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
+        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + dashboardTitle + "/landing");
+        //        getDriver().findElement(By.linkText("Dashboards")).click();
+        //        getDriver().findElement(By.id("ues-view")).click();
+        String y = getDriver().findElement(By.cssSelector(".product-name")).getText();
+        log.info("$$$$" + y);
+        assertEquals(getDriver().findElement(By.cssSelector(".product-name")).getText(), dashboardTitle);
+
     }
-
-//    driver.findElement(By.name("username")).clear();
-//    driver.findElement(By.name("username")).sendKeys("admin");
-//    driver.findElement(By.name("password")).clear();
-//    driver.findElement(By.name("password")).sendKeys("admin");
-//    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.xpath("//div[@id='navbar']/ul/li/a/span/i[2]")).click();
-    driver.findElement(By.id("ues-dashboard-title")).clear();
-    driver.findElement(By.id("ues-dashboard-title")).sendKeys("sample-dashboard");
-    driver.findElement(By.id("ues-dashboard-description")).clear();
-    driver.findElement(By.id("ues-dashboard-description")).sendKeys("This is a sample dashboard");
-    new Select(driver.findElement(By.id("ues-theme-list"))).selectByVisibleText("custom-theme-sample");
-    driver.findElement(By.id("ues-dashboard-create")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.xpath("//div[@id='ues-page-layouts']/div[2]/img")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.xpath("//a[@id='btn-sidebar-gadgets']/i")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.xpath("//a[@id='btn-pages-sidebar']/i")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.xpath("//div[@id='ues-page-layouts']/div[2]/img")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.xpath("//a[@id='btn-sidebar-gadgets']/i")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.linkText("View")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _blank | 30000]]
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.id("dashboard-settings")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=opensearch | ]]
-    driver.findElement(By.cssSelector("input[type=\"checkbox\"]")).click();
-    driver.findElement(By.cssSelector("span.helper")).click();
-
-
-    driver.get(baseUrl + "/portal/login-controller?destination=%2Fportal%2F");
-    driver.findElement(By.name("username")).clear();
-    driver.findElement(By.name("username")).sendKeys("admin");
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("admin");
-    driver.findElement(By.xpath("//button[@type='submit']")).click();
-    driver.findElement(By.xpath("//div[@id='navbar']/ul/li/a/span/i[2]")).click();
-    driver.findElement(By.id("ues-dashboard-title")).clear();
-    driver.findElement(By.id("ues-dashboard-title")).sendKeys("sample-dashboard");
-    driver.findElement(By.id("ues-dashboard-description")).clear();
-    driver.findElement(By.id("ues-dashboard-description")).sendKeys("This is a sample dashboard");
-    new Select(driver.findElement(By.id("ues-theme-list"))).selectByVisibleText("custom-theme-sample");
-    driver.findElement(By.id("ues-dashboard-create")).click();
-    driver.findElement(By.xpath("//div[@id='ues-page-layouts']/div[2]/img")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.xpath("//a[@id='btn-sidebar-gadgets']/i")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.id("pagesButtonlanding")).click();
-    driver.findElement(By.id("btn-pages-sidebar")).click();
-    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-    driver.findElement(By.xpath("//div[@id='ues-page-layouts']/div[2]/img")).click();
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    driver.findElement(By.id("pagesButtonpage0")).click();
-    driver.findElement(By.xpath("//a[@id='btn-sidebar-gadgets']/i")).click();
-    driver.findElement(By.linkText("View")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _blank | 30000]]
-    driver.findElement(By.linkText("Home")).click();
-    driver.findElement(By.linkText("Page 0")).click();
-    driver.findElement(By.id("dashboard-settings")).click();
-    driver.findElement(By.xpath("(//button[@type='button'])[2]")).click();
-    driver.findElement(By.linkText("Default Theme")).click();
-    driver.findElement(By.id("ues-dashboard-saveBtn")).click();
-    driver.findElement(By.linkText("Dashboards")).click();
-    driver.findElement(By.id("ues-view")).click();
-    // ERROR: Caught exception [ERROR: Unsupported command [waitForPopUp | _blank | 30000]]
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
-    // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
-    // ERROR: Caught exception [ERROR: Unsupported command [selectWindow | name=toolbox-panel-iframe-inspector | ]]
-    // ERROR: Caught exception [Error: locator strategy either id or name must be specified explicitly.]
-
-
 
     /**
      * Clean up after running tests
@@ -210,7 +139,8 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
         getDriver().findElement(By.cssSelector("button[type=\"button\"]")).click();
         Thread.sleep(15000);
         try {
-            logoutFromAdminConsole();
+            logout();
+            //logoutFromAdminConsole();
         } finally {
             getDriver().quit();
         }
