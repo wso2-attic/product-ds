08e5b2c5f5586e40e85bdf20c859d5dc608cae3d
prabushi <prabushisamarakoon@gmail.com>
2016-05-26 11:43:49 +0530

    added the test to check whether the custom themes get applied

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/AddCustomThemeToDashboardTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/AddCustomThemeToDashboardTest.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.*;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
 import org.wso2.carbon.automation.engine.frameworkutils.FrameworkPathUtil;
 import org.wso2.ds.ui.integration.util.DSUIIntegrationTest;
@@ -17,15 +15,15 @@ import java.net.MalformedURLException;
 import static org.testng.Assert.assertEquals;
 
 /**
- * Created by prabushi on 5/25/16.
+ * This class check whether the custom theme get applied to the dashboard
  */
 public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
-    private static final Log log = LogFactory.getLog(AddCustomThemeToDashboardTest.class);
     private static final String USER_NAME = "admin";
     private static final String PASSWORD = "admin";
-    private static final String dashboardTitle = "sample-dashboard";
-    private static final String description = "This is a sample dashboard";
+    private static final String DASHBOARD_TITLE = "sample-dashboard";
+    private static final String DESCRIPTION = "This is a sample dashboard";
     private static final String CUSTOM_THEME_NAME = "custom-theme-sample";
+
     //get the path to custom theme carbon archive file
     String systemResourceLocation = FrameworkPathUtil.getSystemResourceLocation();
     String carFilePath = systemResourceLocation + "files/Custom_Theme.car";
@@ -37,6 +35,11 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
             File.separator + "store" + File.separator + "carbon.super" + File.separator + "fs" +
             File.separator + "theme" + File.separator + CUSTOM_THEME_NAME;
 
+    /**
+     * Initializes the class.
+     *
+     * @param userMode user mode
+     */
     @Factory(dataProvider = "userMode") public AddCustomThemeToDashboardTest(TestUserMode userMode) {
         super(userMode);
     }
@@ -68,10 +71,20 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
         getDriver().findElement(By.name("upload")).click();
         getDriver().findElement(By.cssSelector("button[type=\"button\"]")).click();
         Thread.sleep(15000);
+
         //logout from admin console
         logoutFromAdminConsole();
     }
 
+    /**
+     * This test method sets the custom theme for the dashboard and check whether it get applied,
+     * then again change the theme to default theme form the dashboard settings and check whether
+     * the changes get applied.
+     *
+     * @throws XPathExpressionException
+     * @throws MalformedURLException
+     * @throws InterruptedException
+     */
     @Test(groups = "wso2.ds.dashboard", description = "Adding a custom theme when creating a dashboard") public void addCustomThemeToDashboard()
             throws XPathExpressionException, MalformedURLException, InterruptedException {
         //log in to portal
@@ -81,44 +94,37 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
         redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
         getDriver().findElement(By.cssSelector("[href='create-dashboard']")).click();
         getDriver().findElement(By.id("ues-dashboard-title")).clear();
-        getDriver().findElement(By.id("ues-dashboard-title")).sendKeys(dashboardTitle);
-        getDriver().findElement(By.id("ues-dashboard-description")).clear();
-        getDriver().findElement(By.id("ues-dashboard-description")).sendKeys(description);
+        getDriver().findElement(By.id("ues-dashboard-title")).sendKeys(DASHBOARD_TITLE);
+        getDriver().findElement(By.id("ues-dashboard-DESCRIPTION")).clear();
+        getDriver().findElement(By.id("ues-dashboard-DESCRIPTION")).sendKeys(DESCRIPTION);
+
+        //select the custom theme for the dashboard
         new Select(getDriver().findElement(By.id("ues-theme-list"))).selectByVisibleText("custom-theme-sample");
         getDriver().findElement(By.id("ues-dashboard-create")).click();
         selectLayout("default-grid");
 
         //add a page to the dashboard
         redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
-        WebElement webElement = getDriver().findElement(By.id(dashboardTitle.toLowerCase()));
+        WebElement webElement = getDriver().findElement(By.id(DASHBOARD_TITLE.toLowerCase()));
         webElement.findElement(By.cssSelector(".ues-edit")).click();
         addPageToDashboard();
 
-        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + dashboardTitle + "/landing");
-        String x = getDriver().findElement(By.cssSelector(".product-name")).getText();
-        log.info("$$$$" + x);
+        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + DASHBOARD_TITLE + "/landing");
+
+        //check for the dashboard name in the custom theme
         assertEquals(getDriver().findElement(By.cssSelector(".product-name")).getText(),
                 "This is a custom theme title");
 
-        redirectToLocation(DS_HOME_CONTEXT, "dashboard-settings/" + dashboardTitle);
-        //edirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
-        //bElement = getDriver().findElement(By.id(dashboardTitle.toLowerCase()));
-        //getDriver().findElement(By.id("dashboard-settings")).click();
-        //getDriver().findElement(By.xpath("(//button[@type='button'])[2]")).click();
-        //change custom theme to default theme through dashboard settings
-        //getDriver().findElement(By.linkText("Default Theme")).click();
-        //getDriver().findElement(By.id("ues-dashboard-theme")).sendKeys("Default Theme");
+        //reset the theme to the default theme from the dashboard settings
+        redirectToLocation(DS_HOME_CONTEXT, "dashboard-settings/" + DASHBOARD_TITLE);
         getDriver().findElement(By.xpath("(//button[@type='button'])[2]")).click();
         getDriver().findElement(By.linkText("Default Theme")).click();
         getDriver().findElement(By.id("ues-dashboard-saveBtn")).click();
         Thread.sleep(10000);
-        //redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
-        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + dashboardTitle + "/landing");
-        //        getDriver().findElement(By.linkText("Dashboards")).click();
-        //        getDriver().findElement(By.id("ues-view")).click();
-        String y = getDriver().findElement(By.cssSelector(".product-name")).getText();
-        log.info("$$$$" + y);
-        assertEquals(getDriver().findElement(By.cssSelector(".product-name")).getText(), dashboardTitle);
+        redirectToLocation(DS_HOME_CONTEXT, "dashboards/" + DASHBOARD_TITLE + "/landing");
+
+        //check for the name in the default theme
+        assertEquals(getDriver().findElement(By.cssSelector(".product-name")).getText(), DASHBOARD_TITLE);
 
     }
 
@@ -130,19 +136,25 @@ public class AddCustomThemeToDashboardTest extends DSUIIntegrationTest {
      */
     @AfterClass(alwaysRun = true) public void tearDown()
             throws MalformedURLException, XPathExpressionException, InterruptedException {
-        //log in to admin console
-        loginToAdminConsole(USER_NAME, PASSWORD);
 
-        //undeploy custom theme file
-        getDriver().findElement(By.xpath("(//a[contains(text(),'List')])[6]")).click();
-        getDriver().findElement(By.linkText("Delete")).click();
-        getDriver().findElement(By.cssSelector("button[type=\"button\"]")).click();
-        Thread.sleep(15000);
         try {
+            //log ot form the portal
             logout();
-            //logoutFromAdminConsole();
         } finally {
+            //log in to admin console
+            loginToAdminConsole(USER_NAME, PASSWORD);
+
+            //undeploy custom theme file
+            getDriver().findElement(By.xpath("(//a[contains(text(),'List')])[6]")).click();
+            getDriver().findElement(By.linkText("Delete")).click();
+            getDriver().findElement(By.cssSelector("button[type=\"button\"]")).click();
+            Thread.sleep(15000);
+
+            //logout from admin console
+            logoutFromAdminConsole();
+
             getDriver().quit();
         }
+
     }
 }
