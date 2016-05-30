325485b8a642d9184ff1eead5b25eea66f743edb
sinthuja <sinthuja@wso2.com>
2016-05-30 12:43:57 +0530

    Moving the some hard coded strings to constant.

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/common/CustomThemeDeployerTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/common/CustomThemeDeployerTest.java
@@ -20,6 +20,9 @@ public class CustomThemeDeployerTest extends DSUIIntegrationTest {
     private static final String USER_NAME = "admin";
     private static final String PASSWORD = "admin";
     private static final String CUSTOM_THEME_NAME = "custom-theme-sample";
+    private static final String THEMES_CARBON_APP_LOCATION = "files"+ File.separator +"Custom_Theme.car";
+    private static final String THEMES_STORE_PATH = "store" + File.separator + "carbon.super" + File.separator + "fs" +
+            File.separator + "theme";
 
     /**
      * Initializes class
@@ -38,20 +41,10 @@ public class CustomThemeDeployerTest extends DSUIIntegrationTest {
      */
     @Test(groups = "wso2.ds.common", description = "Deploying and un-deploying a custom theme in the admin console")
     public void addDeleteCustomTheme() throws MalformedURLException, XPathExpressionException, InterruptedException {
-
-        //log in to admin console
         loginToAdminConsole(USER_NAME, PASSWORD);
-
-        //get the path to custom theme carbon archive file
         String systemResourceLocation = FrameworkPathUtil.getSystemResourceLocation();
-        String carFilePath = systemResourceLocation + "files/Custom_Theme.car";
-
-        //get the path to store location of custom themes
-        String carbonHome = FrameworkPathUtil.getCarbonHome();
-        String themeStoredLocation = carbonHome + File.separator + "repository" + File.separator + "deployment" +
-                File.separator + "server" + File.separator + "jaggeryapps" + File.separator + "portal" +
-                File.separator + "store" + File.separator + "carbon.super" + File.separator + "fs" +
-                File.separator + "theme" + File.separator + CUSTOM_THEME_NAME;
+        String carFilePath = systemResourceLocation + THEMES_CARBON_APP_LOCATION;
+        String themeStoredLocation = getPortalFilePath(THEMES_STORE_PATH + File.separator + CUSTOM_THEME_NAME);
 
         //check for custom theme availability before deploying the .car file
         File folder = new File(themeStoredLocation);
