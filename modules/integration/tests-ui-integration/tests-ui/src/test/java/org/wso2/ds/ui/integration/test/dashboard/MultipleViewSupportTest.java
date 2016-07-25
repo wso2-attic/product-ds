9188e7346956bfb45e9433e098d062e2d349ace9
Megala <ktumegala@yahoo.com>
2016-07-25 09:06:12 +0530

    Fixing test case failures

--- modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/MultipleViewSupportTest.java
+++ modules/integration/tests-ui-integration/tests-ui/src/test/java/org/wso2/ds/ui/integration/test/dashboard/MultipleViewSupportTest.java
@@ -87,12 +87,14 @@ public class MultipleViewSupportTest extends DSUIIntegrationTest {
      * @throws XPathExpressionException
      */
     @Test(groups = "wso2.ds.dashboard", description = "Checking a dashboard with single view")
-    public void testSingleView() throws MalformedURLException, XPathExpressionException {
+    public void testSingleView() throws MalformedURLException, XPathExpressionException, InterruptedException {
         redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
         getDriver().findElement(By.id(DASHBOARD_TITLE)).findElement(By.cssSelector(".ues-edit")).click();
         String[][] gadgetMappings = {{"publisher", "b"}, {"usa-map", "c"}};
         String script = generateAddGadgetScript(gadgetMappings);
-        getDriver().findElement(By.cssSelector("i.fw.fw-gadget")).click();
+        getDriver().navigate().refresh();
+        selectPane("gadgets");
+        Thread.sleep(2000);
         getDriver().executeScript(script);
         assertTrue(getDriver().findElement(By.id("publisher-0")).isDisplayed(),
                 "Publisher gadget is not displayed in the page");
@@ -150,7 +152,7 @@ public class MultipleViewSupportTest extends DSUIIntegrationTest {
      */
     @Test(groups = "wso2.ds.dashboard", description = "Checking a dashboard with multiple view by creating new views",
             dependsOnMethods = "testCopyView")
-    public void testMultipleNewViews() throws MalformedURLException, XPathExpressionException {
+    public void testMultipleNewViews() throws MalformedURLException, XPathExpressionException, InterruptedException {
         redirectToLocation(DS_HOME_CONTEXT, DS_DASHBOARDS_CONTEXT);
         getDriver().findElement(By.id(DASHBOARD_TITLE)).findElement(By.cssSelector(".ues-edit")).click();
         createNewView("default-grid");
@@ -158,7 +160,9 @@ public class MultipleViewSupportTest extends DSUIIntegrationTest {
         String[][] gadgetMappings = {{"usa-business-revenue", "a"}, {"subscriber", "d"}};
         String[][] gadgetMappingForNewView = {{"publisher", "a"}, {"subscriber", "b"}};
         String script = generateAddGadgetScript(gadgetMappings);
-        getDriver().findElement(By.cssSelector("i.fw.fw-gadget")).click();
+        getDriver().navigate().refresh();
+        selectPane("gadgets");
+        Thread.sleep(2000);
         getDriver().executeScript(script);
         assertTrue(getDriver().findElement(By.id("usa-business-revenue-0")).isDisplayed(),
                 "Publisher gadget is not displayed in the page");
@@ -167,7 +171,9 @@ public class MultipleViewSupportTest extends DSUIIntegrationTest {
         createNewView("single-column");
         clickOnView("view3");
         script = generateAddGadgetScript(gadgetMappingForNewView);
-        getDriver().findElement(By.cssSelector("i.fw.fw-gadget")).click();
+        getDriver().navigate().refresh();
+        selectPane("gadgets");
+        Thread.sleep(2000);
         getDriver().executeScript(script);
         assertTrue(getDriver().findElement(By.id("publisher-0")).isDisplayed(),
                 "Publisher gadget is not displayed in the page");
